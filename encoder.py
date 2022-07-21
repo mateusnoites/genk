df5feafe7be8db06e9ff845adcda0b90556bdfd2
mateus dias :D <82787934+mateusnoites@users.noreply.github.com>
2022-07-20 23:47:49 -0300

    chaves adicionadas.
    chaves adicionadas para aumentar a segurança da criptografia.

--- encoder.py
+++ encoder.py
@@ -1,3 +1,6 @@
+from asyncio.windows_events import NULL
+
+
 letters = [
     "a",
     "b",
@@ -35,7 +38,7 @@ letters = [
 
 prefix = "genk-"
 
-cipher = [
+cypher = [
     "7j",
     "ml",
     "pi",
@@ -73,7 +76,30 @@ cipher = [
 loop = True
 
 while loop:
+    new_cypher = list(cypher)
+
     text = str(input("Insira seu texto: "))
+    keynum = str(input("Insira a chave numérica (deixar em branco para a chave padrão): "))
+
+    if keynum.replace(" ", "") == "":
+        keynum == "000000000000000000000000000000000000000000000000000" # enchendo de zero pra piorar a performance :D
+    
+    final_key = keynum
+
+    keys = list(keynum)
+
+    for i in keys: # dessa parte até a linha 102, nem eu entendo o que eu fiz
+        i = int(i)
+
+        if i != 0:
+            if (i%2) == 0:
+                for i in range(0, i + 1):
+                    new_cypher.reverse()
+                    pass
+            else:
+                new_cypher.sort(reverse=((i + 13)%2 == 0))
+        else:
+            pass
 
     divided = list(text)
 
@@ -81,7 +107,7 @@ while loop:
 
     for i in divided:
         if i in letters:
-            new_text_list[divided.index(i)] = cipher[letters.index(i)]
+            new_text_list[divided.index(i)] = new_cypher[letters.index(i)]
         else:
             new_text_list[divided.index(i)] = "?!"
 
@@ -91,6 +117,7 @@ while loop:
         new_text += i 
 
     print("Seu texto em Genk: " + new_text)
+    print("Sua chave: " + final_key)
     print("")
     res = input("Deseja encriptar mais algum texto? [s/n]: ")
 
