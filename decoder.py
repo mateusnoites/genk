182f35856e01da5c7401c930e5597ffad2c71c22
mateus dias :D <82787934+mateusnoites@users.noreply.github.com>
2022-07-20 23:46:58 -0300

    chaves adicionadas.
    chaves adicionadas para aumentar a segurança da criptografia.

--- decoder.py
+++ decoder.py
@@ -37,7 +37,7 @@ prefix = "genk-"
 
 JUMP = 2
 
-cipher = [
+cypher = [
     "7j",
     "ml",
     "pi",
@@ -76,7 +76,28 @@ loop = True
 
 
 while loop:
-    text = str(input("Insira seu texto em Genk: "))
+    new_cypher = list(cypher)
+
+    text = str(input("Insira o texto em Genk: "))
+    keynum = str(input("Insira a chave numérica (deixar em branco para a chave padrão): "))
+
+    if keynum.replace(" ", "") == "":
+        keynum == "000000000000000000000000000000000000000000000000000" # enchendo de zero pra piorar a performance :D
+
+    keys = list(keynum)
+
+    for i in keys:
+        i = int(i)
+
+        if i != 0:
+            if (i%2) == 0:
+                for i in range(0, i + 1):
+                    new_cypher.reverse()
+                    pass
+            else:
+                new_cypher.sort(reverse=((i + 13)%2 == 0))
+        else:
+            pass
 
     if text.startswith(prefix):
         text = text.replace(prefix, "")
@@ -86,8 +107,8 @@ while loop:
         new_text_list = divided
 
         for i in divided:
-            if i in cipher:
-                new_text_list[divided.index(i)] = letters[cipher.index(i)]
+            if i in new_cypher:
+                new_text_list[divided.index(i)] = letters[new_cypher.index(i)]
             else:
                 new_text_list[divided.index(i)] = "▯"
 
@@ -97,6 +118,7 @@ while loop:
             new_text += i
 
         print("Seu texto traduzido: " + new_text)
+        print("Sua chave: " + keynum)
         print("")
     else:
         print("Isso não é um texto em Genk.")
